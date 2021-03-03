fc74e08cb6e370e831a9031ace83298566df0062
Ian Cossentino <icossentino@gmail.com>
2021-03-03 10:59:48 -0500

    commit tree 12e08890c819d4615e4b6ab0a634dbbfdee68663

--- index.js
+++ index.js
@@ -9,6 +9,51 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(employee) {
+    const record = {}
+    record.firstName = employee[0]
+    record.familyName = employee[1]
+    record.title = employee[2]
+    record.payPerHour = employee[3]
+    record.timeInEvents = []
+    record.timeOutEvents = []
+    return record
+}
+
+function createEmployeeRecords(recordsArray) {
+    return recordsArray.map(e => createEmployeeRecord(e))
+}
+
+function createTimeInEvent(date) {
+    const event = {}
+    event.type = "TimeIn"
+    event.hour = parseInt(date.slice(-4))
+    event.date = date.slice(0, -5)
+    this.timeInEvents.push(event)
+    return this
+}
+  
+function createTimeOutEvent(date) {
+    const event = {}
+    event.type = "TimeOut"
+    event.hour = parseInt(date.slice(-4))
+    event.date = date.slice(0, -5)
+    this.timeOutEvents.push(event)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(e => e.date === date)
+    const timeOut = this.timeOutEvents.find(e => e.date === date)
+    return (timeOut.hour - timeIn.hour)/100
+}
+  
+  
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date)
+    return hoursWorked * this.payPerHour
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +64,22 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function payrollExpense(recordsArray) {
+    const wagesOwed = recordsArray.map(r => allWagesFor.call(r))
+    const reducer = (accum, curr) => accum + curr
+    return wagesOwed.reduce(reducer)
+}
+
+
+function calculatePayroll(recordsArray) {
+    const wagesOwed = recordsArray.map(r => allWagesFor.call(r))
+    const reducer = (accum, curr) => accum + curr
+    return wagesOwed.reduce(reducer)
+}
+
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(e => e.firstName === firstNameString)
 }
